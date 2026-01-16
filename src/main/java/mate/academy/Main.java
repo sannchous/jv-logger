407145a8d54c703c05a75754324590dc94c3d18f
Alexander Tyschenko <qwitelite@gmail.com>
2026-01-16 23:39:19 +0100

    fixed logger.error

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -18,7 +18,8 @@ public class Main {
         try {
             user = authenticationService.login("bob", "1234");
         } catch (AuthenticationException e) {
-            logger.error("Can't login.", e);
+            logger.error("Can't login. Params: user login = {}",
+                    "login", e);
             return;
         }
         OrderService orderService = new OrderServiceImpl();
